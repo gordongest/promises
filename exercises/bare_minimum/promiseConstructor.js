065dea5b002503a665a20f72890821b65155faf3
Nate Foss <nathanielfoss11@gmail.com>
2020-10-27 12:04:00 -0600

    Completion of callback and constructors

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -1,21 +1,32 @@
-/**
- * Implement these promise-returning functions.
- * Any successful value should be made available in the next `then` block chained
- * to the function invocation, while errors should be available in the `catch` block
- */
-
 var fs = require('fs');
 var request = require('request');
 var Promise = require('bluebird');
+var readFile = Promise.promisifyAll(require('fs').readFile);
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+var pluckFirstLineFromFileAsync = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, content) => {
+      if (!err) {
+        var firstLine = content.split('\n')[0];
+        resolve(firstLine);
+      } else {
+        reject(err);
+      }
+    });
+  });
 };
-
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, body) => {
+      if (!err) {
+        resolve(res.statusCode);
+      } else {
+        reject(err);
+      }
+    });
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
